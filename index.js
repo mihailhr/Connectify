4eaa73d48014f944b108247f3eb0090350c09bcb
mihailhr <mishohristov3@gmail.com>
2024-08-19 17:01:22 +0300

    View engine and static folder setup and test

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express=require("express")
+const handlebars=require("express-handlebars")
+const path=require("path")
 
 
 
 const app=express()
-
+app.use(express.static(path.join(__dirname,"public")))
+app.engine("handlebars",handlebars.engine())
+app.set("view engine","handlebars")
+app.set("views",path.join(__dirname,"views"))
 app.get("/",(req,res)=>{
-    res.send("Works")
+    res.render("home")
 })
 app.listen(3000)
