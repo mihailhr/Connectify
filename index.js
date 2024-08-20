0b68abb0e46eca065be9df0d4bfb1cc1530d5620
mihailhr <mishohristov3@gmail.com>
2024-08-20 14:22:05 +0300

    Responsiveness corrections, links to Github and Gmail now work

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ app.use(express.static(path.join(__dirname,"public")))
 app.engine("handlebars",handlebars.engine())
 app.set("view engine","handlebars")
 app.set("views",path.join(__dirname,"views"))
+
+
+
 app.get("/",(req,res)=>{
     res.render("home")
 })
+
+app.get("/register",(req,res)=>{
+    res.render("register")
+})
+
+app.get("/mainFeed",(req,res)=>{
+    res.render("mainFeed")
+})
+
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+
 app.listen(3000)
