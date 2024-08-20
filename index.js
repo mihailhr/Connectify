bbba524aec54a07c8c5f7e521bfe2f2c93913521
mihailhr <mishohristov3@gmail.com>
2024-08-20 15:38:16 +0300

    Connection to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express=require("express")
 const handlebars=require("express-handlebars")
 const path=require("path")
-
-
+const mongoose=require("mongoose")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app=express()
 app.use(express.static(path.join(__dirname,"public")))
@@ -28,4 +29,30 @@ app.get("/about",(req,res)=>{
     res.render("about")
 })
 
+
+const uri = process.env.URI
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(3000)
+
